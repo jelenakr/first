e5f925d5d78a44dc1732b61bb3c0d51090d4175b
Jelena <l.krilova@gmail.com>
2012-09-06 16:28:37 +0100

    initial commit

---
+++ MyMVCApp/MyMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
